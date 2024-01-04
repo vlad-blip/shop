445c1af5f45eb3d4d6cee8e0050ab0c577e3ef88
Vladyslav <ezio2506@gmail.com>
2024-01-04 21:06:51 +0200

    + created main page

---
+++ src/pages/main-page/main-page.jsx
@@ -0,0 +1,10 @@
+import Layout from "../../layouts/layout";
+import styles from "./styles.module.css";
+
+export default function MainPage() {
+  return (
+    <Layout>
+      <main className={styles.container}></main>
+    </Layout>
+  );
+}
