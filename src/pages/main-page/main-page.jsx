57c43c88da667cf2fdd1f631bdb690a8f6b12c0c
Vladyslav <ezio2506@gmail.com>
2024-01-05 18:50:13 +0200

    * added new section

--- src/pages/main-page/main-page.jsx
+++ src/pages/main-page/main-page.jsx
@@ -1,10 +1,16 @@
 import Layout from "../../layouts/layout";
+import Header from "./components/sections/header/header";
+import NewArrivals from "./components/sections/new-arrivals/new-arrivals";
+
 import styles from "./styles.module.css";
 
 export default function MainPage() {
   return (
     <Layout>
-      <main className={styles.container}></main>
+      <main className={styles.container}>
+        <Header />
+        <NewArrivals />
+      </main>
     </Layout>
   );
 }
